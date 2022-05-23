0712a8864ca22e0e7a588a291dea769eac1c016f
Hasnaa Et-taleby <het-tale@e2r2p11.1337.ma>
2022-05-23 02:14:48 +0100

    check_norme

--- swap_algo.c
+++ swap_algo.c
@@ -6,9 +6,14 @@
 /*   By: het-tale <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/22 20:10:26 by het-tale          #+#    #+#             */
-/*   Updated: 2022/05/22 20:38:41 by het-tale         ###   ########.fr       */
+/*   Updated: 2022/05/22 22:49:35 by het-tale         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 #include "libft/libft.h"
+
+t_list	*three_elements(t_list *a, t_list *b)
+{
+	return (a);
+}
